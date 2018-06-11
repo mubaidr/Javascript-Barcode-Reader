65008a496f8ed3850fbed6698be7c4b09953bb94
Muhammad Ubaid Raza <mubaidr@gmail.com>
2018-06-11 10:07:30 +0500

    added source, configs

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 8,
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: ['standard', 'prettier'],
+  globals: {
+    __static: true,
+    fabric: true,
+  },
+  rules: {
+    semi: 0,
+    'linebreak-style': 0,
+    'no-param-reassign': 0,
+    // allow debugger during development
+    'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+  },
+}
