9b53f23096b951c0b4e1f0500a9a183459fede56
Muhammad Ubaid Raza <mubaidr@gmail.com>
2018-08-07 23:35:43 +0500

    [WIP] code-2of5 interleaved

--- src/2of5/index.js
+++ src/2of5/index.js
@@ -16,36 +16,62 @@ module.exports = (lines, type = 'interleaved') => {
   const sequenceBar = []
 
   const barThreshold = Math.ceil(
-    lines.reduce((pre, item) => pre + item, 0) / lines.length
+    lines.reduce((pre, item) => (pre + item) / 2, 0)
   )
 
-  // extract start/ends pair
-  const startChar = lines
-    .splice(0, 6)
-    .filter((item, index) => index % 2 === 0)
-    .map(line => (line > barThreshold ? 'w' : 'n'))
-    .join('')
+  const minBarWidth = Math.ceil(
+    lines.reduce((pre, item) => {
+      if (item < barThreshold) return (pre + item) / 2
+      return pre
+    }, 0)
+  )
 
-  const endChar = lines
-    .splice(lines.length - 5, 5)
-    .filter((item, index) => index % 2 === 0)
-    .map(line => (line > barThreshold ? 'w' : 'n'))
-    .join('')
+  if (type === 'interleaved') {
+    // TODO: finish this
+    console.log(lines, barThreshold, minBarWidth, type)
+    // extract start/ends pair
+    const startChar = lines
+      .splice(0, 4)
+      .map(line => (line < barThreshold ? 'n' : 'w'))
+      .join('')
 
-  if (startChar !== 'wwn' || endChar !== 'wnw') return null
+    const endChar = lines
+      .splice(lines.length - 3, 3)
+      // .filter((item, index) => index % 2 === 0)
+      .map(line => (line < barThreshold ? 'n' : 'w'))
+      .join('')
 
-  // TODO: implement interleaved
-  // Read one encoded character at a time.
-  while (lines.length > 0) {
-    lines
-      .splice(0, 10)
+    console.log(startChar, endChar)
+
+    if (startChar !== 'nnnn' || endChar !== 'wnn') return null
+  } else {
+    // extract start/ends pair
+    const startChar = lines
+      .splice(0, 6)
       .filter((item, index) => index % 2 === 0)
-      .map(line => sequenceBar.push(line > barThreshold ? 'w' : 'n'))
-  }
+      .map(line => (line > barThreshold ? 'w' : 'n'))
+      .join('')
 
-  // Convert bars pattern to integers.
-  while (sequenceBar.length > 0) {
-    code.push(CHAR_SET.indexOf(sequenceBar.splice(0, 5).join('')))
+    const endChar = lines
+      .splice(lines.length - 5, 5)
+      .filter((item, index) => index % 2 === 0)
+      .map(line => (line > barThreshold ? 'w' : 'n'))
+      .join('')
+
+    if (startChar !== 'wwn' || endChar !== 'wnw') return null
+
+    // Read one encoded character at a time.
+    while (lines.length > 0) {
+      lines
+        .splice(0, 10)
+        .filter((item, index) => index % 2 === 0)
+        .map(line => sequenceBar.push(line > barThreshold ? 'w' : 'n'))
+    }
+
+    // Convert bars pattern to integers.
+    while (sequenceBar.length > 0) {
+      code.push(CHAR_SET.indexOf(sequenceBar.splice(0, 5).join('')))
+    }
   }
 
   return code.join('')
