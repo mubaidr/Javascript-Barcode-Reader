b121b5e1b0a7706c442dcc3f6057e67e8a35a800
Muhammad Ubaid Raza <mubaidr@gmail.com>
2019-06-14 18:12:25 -0700

    update gpujs, add test code

--- src/gpu-utilities.js
+++ src/gpu-utilities.js
@@ -1,33 +1,51 @@
 /* eslint-disable func-names */
 
-const GPU = require('gpu.js')
+const { GPU } = require('gpu.js')
 const UTILITIES = require('./utiltities')
 
-function getLines(data, start, end, channels, width) {
-  return data
-}
-
 async function testGpu() {
-  const gpu = new GPU()
+  const gpu = new GPU({
+    mode: 'dev',
+  })
 
   const image =
     'D:\\Current\\Javascript-Barcode-Reader\\docs\\sample-images\\code-93.jpg'
   const { data, width, height } = await UTILITIES.getImageDataFromSource(image)
-  const channels = data.length / (width * height)
+  // const dataLength = data.length
+  // const channels = dataLength / (width * height)
   const start = 0
-  const end = data.length - 1
+  const end = 100
+  const extractedData = data.slice(start, end)
 
-  const ImageDataToLines = gpu
-    .createKernel(function(arr, s, e, c, w) {
-      return getLines(arr[this.thread.x], s, e, c, w)
-    })
-    .setOutput([data.length])
-    .setFunctions([getLines])
+  /* eslint-disable no-shadow */
+  const ImageDataToLines = gpu.createKernelMap(
+    [
+      function add(a, b) {
+        return a + b
+      },
+      function multiply(a, b) {
+        return a * b
+      },
+    ],
+    function(a) {
+      return a[this.thread.x]
+    },
+    { output: [extractedData.length] }
+  )
 
-  console.log(ImageDataToLines(data, start, end, channels, width))
+  /* eslint-enable no-shadow */
+  const result = ImageDataToLines(extractedData)
+
+  return result
 }
 
 testGpu()
+  .then(res => {
+    console.log('Result: ', res)
+  })
+  .catch(err => {
+    console.error(err)
+  })
 
 module.exports = {
   // ImageDataToLines,
