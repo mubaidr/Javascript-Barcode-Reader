f433b1c0dd96fb5da06861961bb7b7a796b86585
Muhammad Ubaid Raza <mubaidr@gmail.com>
2019-08-21 09:48:24 -0700

    add esm builds

--- rollup.config.js
+++ rollup.config.js
@@ -13,11 +13,11 @@ export default [
       globals: {
         jimp: 'window.jimp', // fake jimp to document to avoid jimp load error
       },
-      file: 'dist/javascript-barcode-reader.min.js',
+      file: 'dist/javascript-barcode-reader.umd.min.js',
       format: 'umd',
       name: 'javascriptBarcodeReader',
       sourcemap: true,
-      sourcemapFile: 'dist/javascript-barcode-reader.min.js.map',
+      sourcemapFile: 'dist/javascript-barcode-reader.umd.min.js.map',
     },
     plugins: [
       resolve(),
@@ -36,11 +36,11 @@ export default [
       globals: {
         jimp: 'window.jimp', // fake jimp to document to avoid jimp load error
       },
-      file: 'dist/javascript-barcode-reader.js',
-      format: 'umd',
+      file: 'dist/javascript-barcode-reader.esm.min.js',
+      format: 'esm',
       name: 'javascriptBarcodeReader',
       sourcemap: true,
-      sourcemapFile: 'dist/javascript-barcode-reader.js.map',
+      sourcemapFile: 'dist/javascript-barcode-reader.esm.min.js.map',
     },
     plugins: [
       resolve(),
@@ -49,10 +49,34 @@ export default [
         babelrc: true,
         runtimeHelpers: true,
       }),
+      production && terser(),
     ],
     watch: {
       clearScreen: false,
     },
+  },
+  {
+    external: ['jimp'],
+    input: 'src/index.js',
+    output: {
+      globals: {
+        jimp: 'window.jimp', // fake jimp to document to avoid jimp load error
+      },
+      file: 'dist/javascript-barcode-reader.min.js',
+      format: 'umd',
+      name: 'javascriptBarcodeReader',
+      sourcemap: true,
+      sourcemapFile: 'dist/javascript-barcode-reader.min.js.map',
+    },
+    plugins: [
+      resolve(),
+      commonjs(),
+      babel({
+        babelrc: true,
+        runtimeHelpers: true,
+      }),
+      production && terser(),
+    ],
   },
   {
     external: ['jimp'],
