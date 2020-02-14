ba36b4168faa872985a4142ad6493dd0cad7b4c2
Muhammad Ubaid Raza <mubaidr@gmail.com>
2020-02-14 19:22:21 +0500

    fix build config

---
+++ docs/assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"128":"Class"},"rows":[{"id":0,"kind":128,"name":"DummyClass","url":"classes/dummyclass.html","classes":"tsd-kind-class"}]};
