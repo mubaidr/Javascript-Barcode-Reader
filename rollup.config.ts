5c07d5540f3b51577fd56ebf715e127212532778
Muhammad Ubaid Raza <mubaidr@gmail.com>
2020-03-02 21:40:57 +0500

    fix browser build

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,10 +1,10 @@
 import commonjs from '@rollup/plugin-commonjs'
+import babel from 'rollup-plugin-babel'
 import json from 'rollup-plugin-json'
 import resolve from 'rollup-plugin-node-resolve'
 import sourceMaps from 'rollup-plugin-sourcemaps'
+import { terser } from 'rollup-plugin-terser'
 import typescript from 'rollup-plugin-typescript2'
-
-const pkg = require('./package.json')
 
 const libraryName = 'javascriptBarcodeReader'
 
@@ -12,7 +12,7 @@ export default {
   input: `src/index.ts`,
   output: [
     {
-      file: pkg.main,
+      file: 'dist/javascript-barcode-reader.umd.js',
       name: libraryName,
       format: 'umd',
       sourcemap: true,
@@ -21,12 +21,31 @@ export default {
       },
     },
     {
-      file: pkg.module,
+      file: 'dist/javascript-barcode-reader.umd.min.js',
+      name: libraryName,
+      format: 'umd',
+      sourcemap: true,
+      globals: {
+        jimp: 'Jimp',
+      },
+      plugins: [terser()],
+    },
+    {
+      file: 'dist/javascript-barcode-reader.es5.js',
       format: 'es',
       sourcemap: true,
       globals: {
         jimp: 'Jimp',
       },
+    },
+    {
+      file: 'dist/javascript-barcode-reader.es5.min.js',
+      format: 'es',
+      sourcemap: true,
+      globals: {
+        jimp: 'Jimp',
+      },
+      plugins: [terser()],
     },
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
@@ -39,6 +58,10 @@ export default {
     json(),
     // Compile TypeScript files
     typescript({ useTsconfigDeclarationDir: true }),
+    // use babel
+    babel({
+      exclude: 'node_modules/**',
+    }),
     // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
     commonjs(),
     // Allow node_modules resolution, so you can use 'external' to control
